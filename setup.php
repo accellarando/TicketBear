aec0c18c6f0c5e4869a8ab616745c61e4c4b976a
Ella Moss <emoss@campusstore.utah.edu>
2022-03-16 15:19:05 -0600

    fixed setup.php

--- setup.php
+++ setup.php
@@ -1,17 +1,7 @@
 <?php
-
-namespace Accellarando\TicketBear;
-
-use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
-
-class Setup{
-    public static function run(Event $event){
-        $inject = '\<\?php 
-            require(__DIR__."/../packages/accellarando/ticketbear/src/helpers.php");';
-        $app = __DIR__."/../../../bootstrap/app.php";
-        $contents = file($app);
-        unset($contents[0]);
-        file_put_contents($app,$inject.$file);
-    }
-}
+$inject = '<?php 
+require(__DIR__."/../vendor/accellarando/ticketbear/src/helpers.php");';
+$app = __DIR__."/../../../bootstrap/app.php";
+$contents = file($app);
+unset($contents[0]);
+file_put_contents($app,$inject.implode("\n",$contents));
